0bcf28422396910230de6e223edead9819374f71
Chanhee Jeong <brokenchj@gmail.com>
2023-02-24 13:57:51 +0900

    Initial Commit

---
+++ Somlimee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Somlimee
+//
+//  Created by Chanhee on 2023/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
